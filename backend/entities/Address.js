05cf137c98cf1933861cb277893c1d3d56dc95de
RickardA <rickard.andersson98@gmail.com>
2019-10-01 11:08:46 +0200

    added long and lat values to address model

--- backend/entities/Address.js
+++ backend/entities/Address.js
@@ -5,6 +5,8 @@ let addressSchema = new Schema({
     city: {type: String, required: true},
     country: {type: String, required: true},
     locationId: {type: String, required: false},
+    lat: {type:Number,required:false},
+    long: {type:Number,required:false}
 });
 
 module.exports = db.model('Address', addressSchema);
