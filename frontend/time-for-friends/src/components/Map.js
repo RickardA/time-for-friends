6dc57365384543d931d874f48372d7ea7b8b9d80
RickardA <rickard.andersson98@gmail.com>
2019-10-01 21:09:40 +0200

    error handling fixed

--- frontend/time-for-friends/src/components/Map.js
+++ frontend/time-for-friends/src/components/Map.js
@@ -11,38 +11,54 @@ export default class Map extends Component {
             lng: this.props.long,
         },
         zoom: this.props.zoom,
+        error: false,
     }
 
-
     componentDidMount() {
-        if (this.state.center.lat !== null && this.state.center.long !== null) {
-            let platform = new window.H.service.Platform(this.state);
+        if (this.state.center.lng !== null && this.state.center.lat !== null && typeof this.state.center.lng !== 'undefined' && typeof this.state.center.lat !== 'undefined') {
+            try {
+                let platform = new window.H.service.Platform(this.state);
 
-            let layer = platform.createDefaultLayers();
-            let container = document.getElementById(this.props.mapId);
+                let layer = platform.createDefaultLayers();
+                let container = document.getElementById(this.props.mapId);
 
-            let map = new window.H.Map(
-                container,
-                layer.normal.map, {
-                center: this.state.center,
-                zoom: this.state.zoom
+                let map = new window.H.Map(
+                    container,
+                    layer.normal.map, {
+                    center: this.state.center,
+                    zoom: this.state.zoom
+                }
+                );
+
+                let marker = new window.H.map.Marker(this.state.center)
+
+                map.addObject(marker)
+                this.setErrorState(false);
+            } catch (err) {
+                this.setErrorState(true);
             }
-            );
-
-            let marker = new window.H.map.Marker(this.state.center)
-
-            map.addObject(marker)
+        } else {
+            this.setErrorState(true);
         }
     }
 
+    setErrorState(state) {
+        this.setState((prevState) => {
+            return {
+                ...prevState,
+                error: state,
+            }
+        })
+    }
+
     render() {
-        if (this.state.center.lat !== null && this.state.center.long !== null) {
+        if (this.state.error === false) {
             return (
                 <div id={this.props.mapId} style={{ width: '100%', height: this.props.height, background: 'grey' }}>
                 </div>);
-        }else{
-            return(
-                <img src={noMap} alt="No map available" style={{width:'100%', height:this.props.height}}></img>
+        } else {
+            return (
+                <img src={noMap} alt="No map available" style={{ width: '100%', height: this.props.height }}></img>
             )
         }
     }
