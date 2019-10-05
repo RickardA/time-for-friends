4cb2b20f9c1413cdd33676e96a48517677d47745
RickardA <rickard.andersson98@gmail.com>
2019-10-05 16:02:06 +0200

    map with all friends and added search bar for fun

--- frontend/time-for-friends/src/pages/FriendMap.js
+++ frontend/time-for-friends/src/pages/FriendMap.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import Map from '../components/Map';
+import SearchBar from '../components/SearchBar';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
 
 @inject('store')
 @observer
@@ -11,21 +14,33 @@ class FriendMap extends Component {
     }
 
     render() {
-        if (this.props.store.persons.status === 'done') {
-            return (
-                <Map
-                    mapId="friendMap"
-                    height="100vh"
-                    center={{ lat: 50, lng: 50 }}
-                    persons={this.props.store.persons.data}
-                    infoBubble="true"
-                    zoom="3" />
-            );
-        }else{
-            return(<h1>Loading...</h1>)
-        }
-    }
 
+        let map;
+
+        if (this.props.store.persons.status === 'done') {
+            map = <Map
+                mapId="friendMap"
+                height="70vh"
+                center={{ lat: 50, lng: 50 }}
+                persons={this.props.store.persons.data}
+                infoBubble="true"
+                zoom="3" />
+        } else if (this.props.store.persons.status === 'loading') {
+            map = <Loading />
+        } else {
+            map = <Error title="Something went wrong..." description="Couldn't load your friends, please try again!" />
+        }
+
+        return (
+            <div className="ml-md-5 mr-md-5 mb-4 mr-1 ml-1">
+                <div className="mt-4 mb-4">
+                    <SearchBar />
+                </div>
+
+                {map}
+            </div>
+        );
+    }
 }
 
 export default FriendMap;
